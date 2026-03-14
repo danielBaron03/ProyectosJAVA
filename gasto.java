11eba4c7956c1c49c53e2fcec86cedd20d98c103
danielBaron03 <154362871+danielBaron03@users.noreply.github.com>
2026-03-14 02:59:56 +0100

    Add files via upload
    Herramienta sencilla para administrar datos, desarrollada en JAVA

---
+++ gasto.java
@@ -0,0 +1,11 @@
+public class gasto {
+    
+    String descripción;
+    double cantidad;
+    
+
+    public gasto(String descripción, double cantidad){
+        this.descripción = descripción;
+        this.cantidad = cantidad;
+    }
+}
